e13621240cd2275bc348d3f32db7eff994103935
aleks_pv <apanov08@gmail.com>
2022-05-20 19:50:44 +0400

    refactor: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,17 @@
 const fsPromises = require("fs/promises");
 const path = require("path");
 
-async function copyDir(pathDirCopy, pathDir) {
-await fsPromises.rm(path.join(__dirname, "files-copy"), { recursive: true, force: true});
-await fsPromises.mkdir(pathDir, { recursive: true });
- 
-  const files = await fsPromises.readdir(pathDirCopy, {withFileTypes: true});
+async function copyDir(pathDir, pathDirCopy) {
+await fsPromises.rm(pathDirCopy, { recursive: true, force: true});
+await fsPromises.mkdir(pathDirCopy, { recursive: true }); 
+  const files = await fsPromises.readdir(pathDir, {withFileTypes: true});
   for (file of files) {
-    let srcPath = path.join(pathDirCopy, file.name);
-    let destPath = path.join(pathDir, file.name);
+    let srcPath = path.join(pathDir, file.name);
+    let destPath = path.join(pathDirCopy, file.name);
     if (file.isFile()) {
       await fsPromises.copyFile(
-        path.join(pathDirCopy, `${file.name}`),
-        path.join(pathDir, `${file.name}`)
+        path.join(pathDir, `${file.name}`),
+        path.join(pathDirCopy, `${file.name}`)
       );
     } else {
         await copyDir(srcPath, destPath);        
