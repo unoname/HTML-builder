0e2def50575243381c0e3d527099fe8819cf6af7
aleks_pv <apanov08@gmail.com>
2022-05-21 21:38:42 +0400

    refactor: add new copyStyle

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,27 +1,41 @@
 const path = require("path");
 const fs = require("fs");
+const fsPromises = require('fs/promises')
 
-function copyStyle (srcPath, srcBundle) {
-   const writeStreamFile = fs.createWriteStream(srcBundle);
-const readDir = fs.readdir(srcPath, { withFileTypes: true }, (err, files) => {
-    if (err) throw err;
-    for (let i = 0; i < files.length; i++) {        
-      if (files[i].isFile()) {
-        if (path.extname(`${files[i].name}`) == '.css') {
-          let readStreamFile = fs.createReadStream(
-            path.join(srcPath, `${files[i].name}`)
-          );
-          let data = "";
-          readStreamFile.on("data", chunk => {
-            data += chunk;
-            writeStreamFile.write(data);
-          });
-          data = "";
-        }
+async function copyStyle(srcPath, srcBundle) {
+  const writeStreamFile = fs.createWriteStream(srcBundle);
+  const files = await fsPromises.readdir(srcPath, {withFileTypes: true});
+  for(file of files) {
+    if(file.isFile()) {
+      if(path.extname(`${file.name}`) == '.css') {
+        let readFile = await fsPromises.readFile(path.join(srcPath, `${file.name}`));
+        writeStreamFile.write(readFile);
       }
     }
   }
-); 
 }
+// other solution
+// function copyStyle (srcPath, srcBundle) {
+//    const writeStreamFile = fs.createWriteStream(srcBundle);
+// const readDir = fs.readdir(srcPath, { withFileTypes: true }, (err, files) => {
+//     if (err) throw err;
+//     for (let i = 0; i < files.length; i++) {        
+//       if (files[i].isFile()) {
+//         if (path.extname(`${files[i].name}`) == '.css') {
+//           let readStreamFile = fs.createReadStream(
+//             path.join(srcPath, `${files[i].name}`)
+//           );
+//           let data = "";
+//           readStreamFile.on("data", chunk => {
+//             data += chunk;
+//             writeStreamFile.write(data);
+//           });
+//           data = "";
+//         }
+//       }
+//     }
+//   }
+// ); 
+// }
 
 copyStyle(path.join(__dirname, "styles"), path.join(__dirname, "project-dist", "bundle.css"))
