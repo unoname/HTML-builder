a6998fbb6f40074d4557b49f5403caaac525cde3
aleks_pv <apanov08@gmail.com>
2022-11-11 08:59:20 +0400

    fix: correct output names of files third task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,12 +2,11 @@ const fsPromises = require('fs/promises');
 const path = require('path');
 
 (async function () {
-    const files = await fsPromises.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true});    
-   for(file of files) {
-       if(file.isFile()) {
-           let fileName = file.name.split(".").join(".");         
-        const stat = await fsPromises.stat(path.join(__dirname, 'secret-folder', `${fileName}`));      
-           console.log(file.name.split(".").join(" - ") + " - " + stat.size + 'Bytes')           
+    const files = await fsPromises.readdir(path.join(__dirname, 'secret-folder'));    
+   for(let file of files) {
+		        const stat = await fsPromises.stat(path.join(__dirname, 'secret-folder', file)); 
+       if(stat.isFile()) {        
+					 console.log(path.parse(file).name + " - " + path.parse(file).ext.replace('.', '') + " - " + (stat.size / 1024).toFixed(3) + "kb");           
        }      
     }
 })()
